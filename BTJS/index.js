b0cdee4aa9b650752cda4c361b22e4330571834e
yuu <myduyen14125@gmail.com>
2021-04-06 13:16:51 +0700

    .

--- BTJS/index.js
+++ BTJS/index.js
@@ -1,27 +1,28 @@
 let list = document.getElementById('list');
 let elName = document.getElementById('fullname').value;
+let i;
 let elAge = document.getElementById('age').value;
 let model = document.getElementById("box-model");
 let btn = document.getElementById("btn");
+
 model.style.display = "none";
 
 function myFunction(e) {
     e.preventDefault();
     let elName = document.getElementById('fullname').value;
-    console.log(elName);
-    list.innerHTML += '<li>' + elName + '</li>';
+    let elAge = document.getElementById('age').value;
+    for (i = 0; i < elName.length; i++){
+        if (elName[i] >= 0 && elName[i] <= 9) 
+            model.style.display = "block";
+        else 
+            list.innerHTML += '<li">' + '<pre>' +  "Name: " + elName + "    Age: " + elAge + '</pre>'+ '</li>';
+    }
 }
 
-function check(){
-   if(elName < 'A' || elName > 'z' || elName == null || elName == ""){
-       model.style.display = "block";
-   }
-}
 
 function close(){
     model.style.display = "none";
 }
-
 
 btn.addEventListener("click", function(e) { //async
     close();
