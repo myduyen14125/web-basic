f51aa7fa3a5a02026f619de4071fc7856190e235
yuu <myduyen14125@gmail.com>
2021-04-11 00:09:35 +0700

    10BTJS

--- BTJS3/BT.js
+++ BTJS3/BT.js
@@ -19,6 +19,24 @@ let sub = document.getElementById("sub");
 let clear = document.getElementById("clear");
 let output = document.getElementById("output");
 
+sub.addEventListener("click", function(e){
+    output.style.display = "block";
+    currentDate();
+    fullDate();
+    checkIncreasedDigits();
+    changeString();
+    createString();
+    findInString();
+    checkString();
+    nameOfMonth();
+    longestWordOfString();
+    primeNumber();
+});
+
+clear.addEventListener("click", function(e){
+    output.innerHTML = null;
+})
+
 function currentDate(){
     let output = document.getElementById("output");
     let today = new Date();
@@ -48,7 +66,7 @@ function fullDate(){
 function checkIncreasedDigits(){
     let input = document.getElementById("inputNumber").value;
     let output = document.getElementById("output");
-    let i = 1, dem = 0;
+    let i = 1, count = 0;
     let temp = input[0];
     while(i < input.length){
         if(temp >= input[i]){
@@ -58,10 +76,10 @@ function checkIncreasedDigits(){
         else{
             temp = input[i];
             i++;
-            dem++;
+            count++;
         }
     }
-    if(dem == input.length -1) output.innerHTML += "3. Increased Digits: Yub!" + '<br>';
+    if(count == input.length -1) output.innerHTML += "3. Increased Digits: Yub!" + '<br>';
 }
 
 //Bài 4 : Viết một chương trình JavaScript để thay thế mọi ký tự trong một chuỗi đã
@@ -90,18 +108,102 @@ function createString(){
     let output = document.getElementById("output");
     if(input.length >= 3 && input.length % 2 != 0)
         output.innerHTML += "5. Center substring: " + (input.substring( ((input.length-1)/2)-1, (input.length+3)/2)) + '<br>';
-    else output.innerHTML += "5. String needs odd characters!" + '<br>' + '<br>';
+    else output.innerHTML += "5. String needs odd characters!" + '<br>';
 }
 
-sub.addEventListener("click", function(e){
-    output.style.display = "block";
-    currentDate();
-    fullDate();
-    checkIncreasedDigits();
-    changeString();
-    createString();
-});
 
-clear.addEventListener("click", function(e){
-    output.style.display = "none";
-})
+//Bài 6 : Viết chương trình hiển thị số xuất hiện nhiều lần nhất trong mảng
+function findInString(){
+    let input = document.getElementById("input").value;
+    let save = input ;
+    let max = 0, letter;
+    for (let i = 0; i < input.length; i++){
+        let count = 0;
+        for (let j = 0; j < input.length; j++){
+            if (save[i] == input[j]){
+                count ++;
+                if(count > max) max = count;
+            }
+        }
+        letter = input[i];
+    }
+    if(max == 1) output.innerHTML += "6. String has equal number of each character" + '<br>';
+    else output.innerHTML += "6. Find letters have most in string: "+ letter +". Times: "+ max + '<br>';
+    //console.log(letter);
+}
+
+//Bài 7 : Viết chương trình để kiểm tra xem chuỗi chứa &#39;java&#39; hay không
+function checkString(){
+    let input = document.getElementById("input").value;
+    let output = document.getElementById("output");
+    let count = 0;
+    for (let i = 0; i < input.length; i++){
+        if (input.substring(i, i + 4) == "java"){
+            output.innerHTML += "7. String has 'java'" + '<br>';
+            count = 1; 
+        }
+    }
+    if (count == 0) {
+        output.innerHTML += "7. String doesn't have 'java'." + '<br>';
+    }
+}
+
+function isIncludeJava(str) {
+    let res = str.indexOf("java");
+    if(res != -1) return true;
+    return false;
+}
+//Bài 8 : Viết một hàm để lấy tên tháng từ một số cụ thể ( ví dụ : 1 – Tháng một )
+function nameOfMonth(){
+    let input = document.getElementById("inputMonth").value;
+    let output = document.getElementById("output");
+    switch(input){
+        case '1': output.innerHTML += "8. January" + '</br>'; break;
+        case '2': output.innerHTML += "8. February" + '</br>'; break;
+        case '3': output.innerHTML += "8. March" + '</br>'; break;
+        case '4': output.innerHTML += "8. April" + '</br>'; break;
+        case '5': output.innerHTML += "8. May" + '</br>'; break;
+        case '6':output.innerHTML += "8. June" + '</br>'; break;
+        case '7':output.innerHTML += "8. July" + '</br>'; break;
+        case '8':output.innerHTML += "8. August" + '</br>'; break;
+        case '9': output.innerHTML += "8. September" + '</br>'; break;
+        case '10':output.innerHTML += "8. October" + '</br>'; break;
+        case '11': output.innerHTML += "8. November" + '</br>'; break;
+        case '12':output.innerHTML += "8. December" + '</br>'; break;
+    }
+}
+//Bài 9: Viết một hàm JavaScript nhập một chuỗi và tìm từ dài nhất trong chuỗi
+function longestWordOfString(){
+    let input = document.getElementById("input").value;
+    let output = document.getElementById("output");
+    let words = input.split(' ');//hàm slit(' ') cắt chuỗi thành một mảng các kí tự
+    let maxLength = 0, longestWord;
+    for (let i = 0; i < words.length; i++){
+        if(words[i].length > maxLength){
+            maxLength = words[i].length;
+            longestWord = words[i];
+        }
+    }
+    output.innerHTML += "9. Longest words of string: ";
+    for (let i = 0; i < words.length; i++){
+        if (words[i].length == maxLength) 
+        output.innerHTML += words[i] + " ";
+    }
+    output.innerHTML += '<br>';
+}
+//Bài 10 : Viết chương trình tạo form HTML nhập 2 số và in ra giá trị là số nguyên tố giữa 2 số đó
+function primeNumber(){
+    let a = document.getElementById("inputA").value;
+    let b = document.getElementById("inputB").value;
+    let output = document.getElementById("output");
+    let count = 0, i, j;
+    output.innerHTML += "10. Prime number: ";
+    for (i = a; i <= b; i++){
+        for(j = 2; j <= Math.sqrt(i); j++ ){
+            if(i % j == 0) {count ++;}
+        }
+        if(count == 0) output.innerHTML += i + " ";
+        count = 0;
+    }
+    output.innerHTML += '<br>'
+}
