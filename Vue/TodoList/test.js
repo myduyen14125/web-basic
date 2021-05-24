f1837fab56555ad111aaa4ac1589012a16a237e8
yuu <myduyen14125@gmail.com>
2021-05-24 11:33:55 +0700

    update

---
+++ Vue/TodoList/test.js
@@ -0,0 +1,16 @@
+var list = new Vue({
+    el: "#list",
+    data: {
+        todos: [
+            'Hehe1',
+            'hehe2',
+            
+        ]
+    },
+    methods: {
+        addToList: function(){
+            console.log('you enter your name');
+        },
+    },
+  });
+  
