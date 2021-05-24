c2af6e0ea8f13140f79eaa5cd3b68172fdab24ed
yuu <myduyen14125@gmail.com>
2021-05-24 11:53:44 +0700

    update

--- Vue/TodoList/test.js
+++ Vue/TodoList/test.js
@@ -1,16 +1,32 @@
 var list = new Vue({
     el: "#list",
     data: {
+        selectedIndex: null,
+        isEditing: false,
+        todo: '',
         todos: [
             'Hehe1',
             'hehe2',
-            
+
         ]
     },
     methods: {
-        addToList: function(){
-            console.log('you enter your name');
+        storeTodo: function(){
+            this.todos.push(this.todo)
+            this.todo = ''
         },
+        editTodo(index, todo){
+            this.todo = todo
+            this.selectedIndex = index
+            this.isEditing = true
+        },
+        updateTodo(){
+            this.todos.splice(this.selectedIndex, 1, this.todo)
+            this.isEditing = false
+        },
+        deleteTodo(index){
+            this.todos.splice(index, 1)
+        }
     },
   });
   
