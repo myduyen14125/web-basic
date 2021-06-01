98f33e838e27966ec3d0010c17d3d40823753899
yuu <myduyen14125@gmail.com>
2021-06-02 06:50:06 +0700

    update cv

--- CV/home.js
+++ CV/home.js
@@ -3,6 +3,7 @@ let headerBox = document.getElementById("header-box");
 let count = 0;
 let x = window.matchMedia("(max-width: 1200px)");
 
+// Resize screen 
 function reszieScreen(x){
     if(x.matches){
         headerBox.style.transform = "translate(-260px)";
@@ -30,4 +31,113 @@ hiddenBar.addEventListener("click", function(e){
         hiddenBar.innerHTML = '<div class="fa fa-bars">' + '</div>';        
     }
 
+})
+
+// JS choose each page
+let homeButton = document.getElementById("home-button"); 
+let aboutButton = document.getElementById("about-button");
+let resumeButton = document.getElementById("resume-button");
+let portfolioButton = document.getElementById("portfolio-button");
+let blogButton = document.getElementById("blog-button");
+let contactButton = document.getElementById("contact-button");
+
+let homePage = document.getElementById("home-page");
+let aboutPage = document.getElementById("about-page");
+let resumePage = document.getElementById("resume-page");
+let portfolioPage = document.getElementById("portfolio-page");
+let blogPage = document.getElementById("blog-page");
+let contactPage = document.getElementById("contact-page");
+
+homeButton.style.backgroundColor = "#0f7bff";
+
+homeButton.addEventListener("click", function(){
+    homeButton.style.backgroundColor = "#0f7bff";
+    aboutButton.style.backgroundColor = "#191d2b";
+    resumeButton.style.backgroundColor = "#191d2b";
+    portfolioButton.style.backgroundColor = "#191d2b";
+    blogButton.style.backgroundColor = "#191d2b";
+    contactButton.style.backgroundColor = "#191d2b";
+
+    homePage.style.display = "block";
+    aboutPage.style.display = "none";
+    resumePage.style.display = "none";
+    portfolioPage.style.display = "none";
+    blogPage.style.display = "none";
+    contactPage.style.display = "none";
+
+})
+aboutButton.addEventListener("click", function(){
+    homeButton.style.backgroundColor = "#191d2b";
+    aboutButton.style.backgroundColor = "#0f7bff";
+    resumeButton.style.backgroundColor = "#191d2b";
+    portfolioButton.style.backgroundColor = "#191d2b";
+    blogButton.style.backgroundColor = "#191d2b";
+    contactButton.style.backgroundColor = "#191d2b";
+
+    homePage.style.display = "none";
+    aboutPage.style.display = "block";
+    resumePage.style.display = "none";
+    portfolioPage.style.display = "none";
+    blogPage.style.display = "none";
+    contactPage.style.display = "none";
+})
+resumeButton.addEventListener("click", function(){
+    homeButton.style.backgroundColor = "#191d2b";
+    aboutButton.style.backgroundColor = "#191d2b";
+    resumeButton.style.backgroundColor = "#0f7bff";
+    portfolioButton.style.backgroundColor = "#191d2b";
+    blogButton.style.backgroundColor = "#191d2b";
+    contactButton.style.backgroundColor = "#191d2b";
+
+    homePage.style.display = "none";
+    aboutPage.style.display = "none";
+    resumePage.style.display = "block";
+    portfolioPage.style.display = "none";
+    blogPage.style.display = "none";
+    contactPage.style.display = "none";
+})
+portfolioButton.addEventListener("click", function(){
+    homeButton.style.backgroundColor = "#191d2b";
+    aboutButton.style.backgroundColor = "#191d2b";
+    resumeButton.style.backgroundColor = "#191d2b";
+    portfolioButton.style.backgroundColor = "#0f7bff";
+    blogButton.style.backgroundColor = "#191d2b";
+    contactButton.style.backgroundColor = "#191d2b";
+
+    homePage.style.display = "none";
+    aboutPage.style.display = "none";
+    resumePage.style.display = "none";
+    portfolioPage.style.display = "block";
+    blogPage.style.display = "none";
+    contactPage.style.display = "none";
+})
+blogButton.addEventListener("click", function(){
+    homeButton.style.backgroundColor = "#191d2b";
+    aboutButton.style.backgroundColor = "#191d2b";
+    resumeButton.style.backgroundColor = "#191d2b";
+    portfolioButton.style.backgroundColor = "#191d2b";
+    blogButton.style.backgroundColor = "#0f7bff";
+    contactButton.style.backgroundColor = "#191d2b";
+
+    homePage.style.display = "none";
+    aboutPage.style.display = "none";
+    resumePage.style.display = "none";
+    portfolioPage.style.display = "none";
+    blogPage.style.display = "block";
+    contactPage.style.display = "none";
+})
+contactButton.addEventListener("click", function(){
+    homeButton.style.backgroundColor = "#191d2b";
+    aboutButton.style.backgroundColor = "#191d2b";
+    resumeButton.style.backgroundColor = "#191d2b";
+    portfolioButton.style.backgroundColor = "#191d2b";
+    blogButton.style.backgroundColor = "#191d2b";
+    contactButton.style.backgroundColor = "#0f7bff";
+
+    homePage.style.display = "none";
+    aboutPage.style.display = "none";
+    resumePage.style.display = "none";
+    portfolioPage.style.display = "none";
+    blogPage.style.display = "none";
+    contactPage.style.display = "block";
 })
